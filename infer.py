d2899bdf1cf3b3d40d5dd38d11873ac4612b24d2
Asha-Gutlapalli <ashagutlapalli0316@gmail.com>
2021-06-11 16:32:20 +0530

    Sample inference file

---
+++ infer.py
@@ -0,0 +1,30 @@
+import os
+
+import torch
+
+from stylegan2.utils import audio_features, noise, image_noise, timestamped_filename
+from stylegan2.model import StyleGAN2Model
+
+# file paths
+audio_path = './sample.wav'
+model_path = '/home/asha/StyleGAN2-Art/.models/trippy/model_45.pt'
+
+# get ratios from audio features for interpolation
+ratios = audio_features(file_path = audio_path)
+
+# load model
+model = StyleGAN2Model(model_path = model_path)
+
+# generate images and sync audio
+model.generate_interpolation(name = timestamped_filename(), ratios = ratios, sync_audio = True, audio_path = audio_path)
+
+#noise
+n = 10
+latent_dim = 512
+image_size = 128
+
+noise_z = torch.randn(1, latent_dim).repeat(n, 1)
+noise = torch.FloatTensor(n, image_size, image_size, 1).uniform_(0., 1.)
+
+# generate images from uniform changes in latent space
+model.generate_latent(name = timestamped_filename(), noise_z = noise_z, noise = noise)
